ff3e5cf30a5b2a5b9eb8d30961828da77e6e789d
lbula <bulalalisa@gmail.com>
2023-12-30 03:09:42 +0300

    css updated

--- frontendFiles/src/sections/footer/footer.js
+++ frontendFiles/src/sections/footer/footer.js
@@ -8,10 +8,12 @@ const Footer = () => {
     <footer>
       <div className="container footer__container">
         <div className="footer__items nav__cont">
-          <img src={myphoto} alt="my_photo" className="nav__logo" />
+          <a href="index.html">
+            <img src={myphoto} alt="Logo" className="nav__logo" />
+          </a>{' '}
           <ul className="nav__menu nav__menu__footer">
             {links.map((fLink) => (
-              <li key={fLink.id}>
+              <li key={fLink.id} className="item">
                 <a href={fLink.link}>{fLink.title}</a>
               </li>
             ))}
