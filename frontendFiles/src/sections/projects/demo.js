6e19dccec4c8f14fab8643ba0d2805f39c5619f1
Lalisa <bulalalisa@gmail.com>
2024-05-13 22:47:57 -0500

    added

---
+++ frontendFiles/src/sections/projects/demo.js
@@ -0,0 +1,16 @@
+const Demo=()=>{
+return(
+    <section id="project">
+  <div className="left">
+<h3></h3>
+<h4></h4>
+<p></p>
+<link></link>
+  </div>
+  <div className="right">
+    <img></img>
+  </div>
+    </section>
+)
+}
+export default Demo;
